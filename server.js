34f2ed64f628c90f70777cff40ac0d91859e83e5
MrA2411 <aaespinoza87@gmail.com>
2026-03-30 17:31:15 -0300

    Actualizacion de mi backend

---
+++ server.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Hola Andres, bienvenido a tu nuevo servidor');
+});
+
+app.listen(port, () => {
+  console.log(`Estoy escuchando en el puerto ${port}`);
+});
+
+console.log(process.env);
